dadd8eaff4fe341c33edee071f04a43c415a7745
tchkoidze <ilia.chkoidze12@gmail.com>
2022-12-26 04:15:41 -0800

    change <script src><script> location after body element in a index.html fil and in the kj.js file use async await method to roceive followers list and place in dom.

--- d/kj.js
+++ d/kj.js
@@ -1,26 +1,63 @@
 "use strict mode";
+// const submitButton = document.getElementById("submit");
+// const loginInput = document.getElementById("login");
+// submitButton.addEventListener("click", (event) => {
+//   event.preventDefault();
 
-window.onload = function(){
+//   const login = loginInput.value;
+//   fetchUserFollowers(login);
+// });
 
-  const submitButton = document.getElementById("submit");
-  const loginInput = document.getElementById("login");
-  submitButton.addEventListener("click", (event) => {
-    event.preventDefault();
-  
-    const login = loginInput.value;
-  
-    let apiUrl = `https://api.github.com/users/${login}/followers`;
-    fetch(apiUrl).then((response) => {
-      response.json().then((users) => {
-        console.log(users);
-  
-        const userListElement = document.getElementById("users-list");
-        let userListTemplate = "";
-        users.map((user) => {
-          userListTemplate += `<li> <img src=${user.avatar_url}> ${user.login} </li>`;
-        });
-        userListElement.innerHTML = userListTemplate;
+// const fetchUserFollowers = (login) => {
+//   let apiUrl = `https://api.github.com/users/${login}/followers`;
+//   fetch(apiUrl).then((response) => {
+//     response.json().then((users) => {
+//       console.log(users);
+
+//       const userListElement = document.getElementById("users-list");
+//       let userListTemplate = "";
+//       users.map((user) => {
+//         userListTemplate += `<li> <img src=${user.avatar_url}> ${user.login} </li>`;
+//       });
+//       userListElement.innerHTML = userListTemplate;
+//     });
+//   });
+// };
+
+const submitButton = document.getElementById("submit");
+const loginInput = document.getElementById("login");
+submitButton.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  const login = loginInput.value;
+  const users =   fetchUserFollowers(login);
+  console.log(users);
+
+  const userListElement = document.getElementById("users-list");
+      let userListTemplate = "";
+      users.map((user) => {
+        userListTemplate += `<li> <img src=${user.avatar_url}> ${user.login} </li>`;
       });
-    });
-  });
-}
+      userListElement.innerHTML = userListTemplate;
+});
+
+const fetchUserFollowers = async (login) => {
+  let apiUrl = `https://api.github.com/users/${login}/followers`;
+  
+  let response = await fetch(apiUrl);
+  let json= response.json();
+
+  return json;
+
+  // return fetch(apiUrl).then((response) => {
+  //   response.json().then((users) => {
+  //     return users;
+  //     console.log(users);
+
+      
+  //   });
+  // });
+};
+
+
+
